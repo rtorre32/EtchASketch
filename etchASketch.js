84e0abee972922833e9356d841b7d468896d82c6
Ramon <torres28ramon@gmail.com>
2022-03-24 15:06:46 -0700

    Have a grid, but still need to figure out math to set the grid properly with different sizes, works manually right now

--- etchASketch.js
+++ etchASketch.js
@@ -1,18 +1,20 @@
 const canvasColor = "#FFFAFA";
 const canvas = document.querySelector(".canvas");
 const controls = document.querySelector(".controls");
+const area = (canvas.clientHeight * canvas.clientWidth);
+
+let size = 16;
 
 canvas.style.background = canvasColor;
 // controls.style.background = canvasColor;
 
-console.log(canvas.clientWidth);
-console.log(canvas.clientHeight);
 
-for(let i = 0; i < 500 * 500; i++){
+//  27 28 256
+for(let i = 0; i <756; i++){
     let pixel = document.createElement("div");
+    pixel.style.flex = "1 1 auto";
     pixel.style.border = "1px solid black";
-    pixel.style.width = "64px";
-    pixel.style.height = "64px";
-
+    pixel.style.width = "16px";
+    pixel.style.height = "16px";
     canvas.append(pixel);
 }
