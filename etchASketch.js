e3f1cd6cd6c527602b7d27f469ce79ec67518e36
Ramon <torres28ramon@gmail.com>
2022-03-24 19:08:50 -0700

    Math works for size of 50, still need to test other sizes

--- etchASketch.js
+++ etchASketch.js
@@ -7,27 +7,31 @@ const slider = document.querySelector(".slider");
 const sliderOutput = document.querySelector("output");
 
 let total = slider.value;
+let size = canvas.clientWidth / total;
+console.log(size);
 
 //Slider Input Handling
 sliderOutput.innerHTML = slider.value;
 slider.addEventListener("input", function(){
     sliderOutput.innerHTML = slider.value;
+    total = slider.value;
 })
-
-//
-
-let size = 16;
 
 canvas.style.background = canvasColor;
 // controls.style.background = canvasColor;
 
 
 //  27 28 256
-for(let i = 0; i <756; i++){
+for(let i = 0; i <total * total; i++){
     let pixel = document.createElement("div");
-    pixel.style.flex = "1 1 auto";
+    pixel.style.flex = "auto";
+    pixel.style.boxSizing = "border-box";
     pixel.style.border = "1px solid black";
     pixel.style.width = ""+size+"px";
     pixel.style.height = ""+size+"px";
+
+    // pixel.addEventListener("mouseenter" = () =>{
+    //     // pixel.background = "black";
+    // })
     canvas.append(pixel);
 }
