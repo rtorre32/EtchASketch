c8d0c0cedaef8b81756091e5b38ec641ac335f9d
Ramon <torres28ramon@gmail.com>
2022-03-24 19:43:52 -0700

    Reset button can now create a new grid with the new slider input given

--- etchASketch.js
+++ etchASketch.js
@@ -1,17 +1,23 @@
 
 const canvasColor = "#FFFAFA";
+const colorBlack = "#000000";
 const canvas = document.querySelector(".canvas");
 const controls = document.querySelector(".controls");
 const area = (canvas.clientHeight * canvas.clientWidth);
 const slider = document.querySelector(".slider");
 const sliderOutput = document.querySelector("output");
+const resetButton = document.querySelector(".reset");
 
+let cellColor = colorBlack;
 let total = slider.value;
-let size = canvas.clientWidth / total;
+// let size = canvas.clientWidth / total;
+let size;
 
-
-
-
+//button handler
+resetButton.addEventListener("click", () => {
+    deleteGrid();
+    createGrid();
+})
 
 //Slider Input Handling
 sliderOutput.innerHTML = slider.value;
@@ -21,20 +27,41 @@ slider.addEventListener("input", function(){
 })
 
 canvas.style.background = canvasColor;
-// controls.style.background = canvasColor;
 
-
-//  27 28 256
-for(let i = 0; i <total * total; i++){
-    let pixel = document.createElement("div");
-    pixel.style.flex = "auto";
-    pixel.style.boxSizing = "border-box";
-    pixel.style.border = "1px solid black";
-    pixel.style.width = ""+size+"px";
-    pixel.style.height = ""+size+"px";
-
-    pixel.addEventListener("mouseenter", function(){
-        pixel.style.background = "black";
-    })
-    canvas.append(pixel);
+function clearGrid(){
+    const cells = document.querySelectorAll("div.cell");
+    cells.forEach(cell => cell.style.background = canvasColor);
 }
+
+function deleteGrid(){
+    const cells = document.querySelectorAll("div.cell");
+    cells.forEach(cell => canvas.removeChild(cell));
+}
+
+
+
+
+function createGrid(){
+
+    size = canvas.clientHeight / total;
+
+    console.log("total: " + total);
+    console.log("size: " + size);
+
+    for(let i = 0; i <total * total; i++){
+        let pixel = document.createElement("div");
+        pixel.setAttribute("class", "cell");
+        pixel.style.flex = "auto";
+        pixel.style.boxSizing = "border-box";
+        pixel.style.border = "1px solid black";
+        pixel.style.width = ""+size+"px";
+        pixel.style.height = ""+size+"px";
+    
+        pixel.addEventListener("mouseenter", function(){
+            pixel.style.background = cellColor;
+        })
+        canvas.append(pixel);
+    }
+}
+
+createGrid();
