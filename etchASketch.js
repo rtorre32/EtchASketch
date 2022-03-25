cf71bdc7270c68791237a8c04437a2ad83c760c9
Ramon <torres28ramon@gmail.com>
2022-03-24 18:49:04 -0700

    Update output when slider is moved

--- etchASketch.js
+++ etchASketch.js
@@ -1,7 +1,20 @@
+
 const canvasColor = "#FFFAFA";
 const canvas = document.querySelector(".canvas");
 const controls = document.querySelector(".controls");
 const area = (canvas.clientHeight * canvas.clientWidth);
+const slider = document.querySelector(".slider");
+const sliderOutput = document.querySelector("output");
+
+let total = slider.value;
+
+//Slider Input Handling
+sliderOutput.innerHTML = slider.value;
+slider.addEventListener("input", function(){
+    sliderOutput.innerHTML = slider.value;
+})
+
+//
 
 let size = 16;
 
@@ -14,7 +27,7 @@ for(let i = 0; i <756; i++){
     let pixel = document.createElement("div");
     pixel.style.flex = "1 1 auto";
     pixel.style.border = "1px solid black";
-    pixel.style.width = "16px";
-    pixel.style.height = "16px";
+    pixel.style.width = ""+size+"px";
+    pixel.style.height = ""+size+"px";
     canvas.append(pixel);
 }
